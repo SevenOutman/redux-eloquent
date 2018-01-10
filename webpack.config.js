6138934a7c857580b73a0a3f89319127d8911a1b
Doma <leishenghao@126.com>
2018-01-10 10:47:10 +0800

    webpack build

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    path.join(__dirname, 'src/index'),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'redux-eloquent.min.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'stage-0'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        drop_console: false // eslint-disable-line
+      },
+    }),
+    new webpack.BannerPlugin({ banner: `Last update: ${new Date().toString()}` }),
+  ],
+};
